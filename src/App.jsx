d9619133bc627d98065c5252088784fba1c87047
Marwan Mohamed <mac@192.168.1.66>
2026-04-16 17:51:40 -0700

    the localStorage update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+
+
 
 
 
@@ -6,6 +8,31 @@ export default function App(){
   const [input,setInput]=useState("")
   const [todos, setTodos]=useState([])
   const [filter ,setFilter]=useState('all')
+  const [darkMood , setDarkMood]=useState(false)
+
+
+
+  useEffect(()=>{
+    const saved=localStorage.getItem("themes")
+    if(saved ){
+      setDarkMood(JSON.parse(saved))
+    }
+
+  },[])
+  useEffect(()=>{
+    localStorage.setItem("themes", JSON.stringify(darkMood))
+  }, [darkMood])
+
+  useEffect(()=>{
+    const savedTodos=localStorage.getItem("todos")
+    if(savedTodos){
+      setTodos(JSON.parse(savedTodos))
+    }
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todos))
+  },[todos])
 
   function handleAdd(){
     if(!input.trim()) return
@@ -44,36 +71,40 @@ export default function App(){
   )
 
   return(
-    <div className="bg-gray-200 min-h-screen ">
+    <div className={`${darkMood?"bg-gray-900  min-h-screen": "bg-gray-200 min-h-screen "} max-w-4xl mx-auto`}>
       <div className="relative ">
-        <img src="/images/bg-mobile-dark.jpg" alt=""  className="w-full md:hidden"/>
+        <img src={darkMood ?"/images/bg-mobile-dark.jpg" :"/images/bg-mobile-light.jpg"} alt=""  className="w-full md:hidden"/>
         <img src="/images/bg-desktop-light.jpg" alt="" className="hidden md:flex w-full"/>
       </div>
       <div className="p-4 space-y-6 md:w-1/2  ">
       <div className="absolute top-20 justify-between  p-6 items-center flex w-full md:w-1/2">
         <h1 className="text-xl font-extrabold text-white">Todo App</h1>
-        <div><img src="/images/icon-moon.svg" alt="" /></div>
+        <div><img src={ darkMood?"/images/icon-sun.svg"  :"/images/icon-moon.svg"} alt=""
+        className="cursor-pointer"
+        onClick={()=>setDarkMood(!darkMood)}
+        /></div>
       </div>
+      <div className="space-y-6 mx-auto max-w-xl px-4 w-full">
 
-      <div className="bg-white p-4 shadow-lg rounded-lg  flex  justify-between items-center ">
+      <div className="bg-white p-4 shadow-lg rounded-lg  flex  justify-between items-center   ">
         <div className="flex gap-4">
         <input type="radio"   className="p-2"/>
         <input type="text"
         placeholder="create a new todo..."
         value={input}
-        className="w-full"
+        className="focus:outline-none w-[100%]"
         onChange={(e)=>setInput(e.target.value)}
         />
         </div>
         <button className="bg-sky-400 rounded-lg p-2 text-white" onClick={handleAdd}>Add</button>
       </div>
-      <div className="space-y-4 bg-white rounded-lg p-4 shadow-xl ">
+      <div className={`space-y-4 ${darkMood ?"bg-gray-800 text-white":"bg-white text-black"} rounded-lg p-4 shadow-xl `}>
         {
           todos.length===0?(
-            <p className="text-center text-gray-400 ">Not dotos add yets</p>
+            <p className="text-center text-gray-400 ">Not todos add yets</p>
           ):(
             filterTodos.map((todo)=>(
-              <div key={todo.id} className="flex w-full justify-between items-center border-b border-gray-300 space-y-4 ">
+              <div key={todo.id} className="flex w-full justify-between items-center border-b border-gray-300 space-y-4  ">
                 <div className="flex gap-4 ">
           <img src="/images/icon-check.svg" alt="" className={todo.completed?"shadow-xl bg-purple-500 rounded-full p-2" :"bg-sky-500 rounded-full p-2"} onClick={()=>toggled(todo.id)} />
             <p className={todo.completed?"line-through text-gray-400":"" }>{todo.text}</p>
@@ -100,6 +131,8 @@ export default function App(){
         <button onClick={()=>setFilter("completed")}  className="hover:text-sky-500 focus:text-sky-500 transition "> Completed</button>
       </div>
       <h1 className="text-center text-gray-400">drag and drop to reorder list</h1>
+      </div>
+
       </div>
 
 
