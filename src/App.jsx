1634ceb1ab59ffe50180ab866c8fde52f8ecce06
Marwan Mohamed <mac@192.168.1.66>
2026-04-16 21:10:27 -0700

    fix localStorage return

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,14 @@ import { useEffect, useState } from "react"
 
 export default function App(){
   const [input,setInput]=useState("")
-  const [todos, setTodos]=useState([])
+  const [todos, setTodos]=useState(()=>{
+   return  JSON.parse(localStorage.getItem("todos") || [])
+  })
   const [filter ,setFilter]=useState('all')
-  const [darkMood , setDarkMood]=useState(false)
+  const [darkMood , setDarkMood]=useState(()=>{
+    return JSON.parse(localStorage.getItem("themes") || false)
+  })
+
 
 
 
@@ -16,6 +21,7 @@ export default function App(){
     const saved=localStorage.getItem("themes")
     if(saved ){
       setDarkMood(JSON.parse(saved))
+
     }
 
   },[])
@@ -71,28 +77,31 @@ export default function App(){
   )
 
   return(
-    <div className={`${darkMood?"bg-gray-900  min-h-screen": "bg-gray-200 min-h-screen "} max-w-4xl mx-auto`}>
-      <div className="relative ">
+    <div className={`${darkMood?"bg-gray-900  min-h-screen": "bg-gray-200 min-h-screen "} relative flex justify-center items-center`}>
+      <div className="  absolute top-0 w-full">
         <img src={darkMood ?"/images/bg-mobile-dark.jpg" :"/images/bg-mobile-light.jpg"} alt=""  className="w-full md:hidden"/>
-        <img src="/images/bg-desktop-light.jpg" alt="" className="hidden md:flex w-full"/>
+        <img src={ darkMood? "/images/bg-desktop-dark.jpg":"/images/bg-desktop-light.jpg"} alt="" className="hidden md:flex w-full"/>
       </div>
-      <div className="p-4 space-y-6 md:w-1/2  ">
-      <div className="absolute top-20 justify-between  p-6 items-center flex w-full md:w-1/2">
-        <h1 className="text-xl font-extrabold text-white">Todo App</h1>
+
+
+
+        <div className=" space-y-4  absolute z-10 top-10 mx-auto md:w-1/3 w-full p-4">
+
+      <div className="    p-6  flex justify-between">
+        <h1 className="text-2xl tracking-widest font-extrabold text-white">Todo </h1>
         <div><img src={ darkMood?"/images/icon-sun.svg"  :"/images/icon-moon.svg"} alt=""
         className="cursor-pointer"
-        onClick={()=>setDarkMood(!darkMood)}
-        /></div>
+        onClick={()=>setDarkMood(!darkMood)}/>
+        </div>
       </div>
-      <div className="space-y-6 mx-auto max-w-xl px-4 w-full">
 
-      <div className="bg-white p-4 shadow-lg rounded-lg  flex  justify-between items-center   ">
+      <div className={` p-2 shadow-lg rounded-lg  flex  justify-between items-center  ${darkMood?"bg-gray-800 text-white": "bg-white text-black"} `}>
         <div className="flex gap-4">
         <input type="radio"   className="p-2"/>
         <input type="text"
         placeholder="create a new todo..."
         value={input}
-        className="focus:outline-none w-[100%]"
+        className={`focus:outline-none w-full bg-transparent ${darkMood?"text-white":"text-black"}`}
         onChange={(e)=>setInput(e.target.value)}
         />
         </div>
@@ -104,9 +113,9 @@ export default function App(){
             <p className="text-center text-gray-400 ">Not todos add yets</p>
           ):(
             filterTodos.map((todo)=>(
-              <div key={todo.id} className="flex w-full justify-between items-center border-b border-gray-300 space-y-4  ">
+              <div key={todo.id} className="flex w-full justify-between items-center border-b border-gray-300 space-y-4 p-2 ">
                 <div className="flex gap-4 ">
-          <img src="/images/icon-check.svg" alt="" className={todo.completed?"shadow-xl bg-purple-500 rounded-full p-2" :"bg-sky-500 rounded-full p-2"} onClick={()=>toggled(todo.id)} />
+          <img src="/images/icon-check.svg" alt="" className={todo.completed?"shadow-xl bg-purple-500 rounded-full p-2" :"cursor-pointer bg-sky-500 rounded-full p-2"} onClick={()=>toggled(todo.id)} />
             <p className={todo.completed?"line-through text-gray-400":"" }>{todo.text}</p>
         </div>
         <div><img
@@ -125,16 +134,13 @@ export default function App(){
         }
 
       </div>
-      <div className="bg-white p-4 rounded-lg justify-center items-center  gap-4 w-full flex shadow">
+      <div className="bg-white p-4 rounded-lg justify-center items-center  gap-4 w-full flex shadow  " >
         <button onClick={()=>setFilter("all")}  className="hover:text-sky-500 focus:text-sky-500 transition "  >All</button>
         <button onClick={()=>setFilter("active")}  className="hover:text-sky-500 focus:text-sky-500 transition ">Active</button>
         <button onClick={()=>setFilter("completed")}  className="hover:text-sky-500 focus:text-sky-500 transition "> Completed</button>
       </div>
       <h1 className="text-center text-gray-400">drag and drop to reorder list</h1>
       </div>
-
-      </div>
-
 
 
     </div>
